5c3721060dde3d98583a45e3aa41b07bb3ea5a0c
elemson <toluwani@juststart.dev>
2020-10-03 20:03:07 +0300

    fixed db host name

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost:3306', 'elemlsbm_elemson', '%-5T)g#[hzc!','elemlsbm_ultravpn_db');
+$conn = mysqli_connect('localhost', 'elemlsbm_elemson', '%-5T)g#[hzc!','elemlsbm_ultravpn_db');
 //check connection
     if (mysqli_connect_errno()) {
         echo 'Connection error: ' . mysqli_connect_errno();
