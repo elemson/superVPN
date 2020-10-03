504b41a7867bef74e917536bd713edd08c5bc2c8
elemson <toluwani@juststart.dev>
2020-10-03 11:30:02 +0300

    index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+
+	<!-- Header -->
+	<?php require('components/head.inc.php'); ?>
+	<?php require('components/navbar.php'); ?>
+    <?php require('components/header.php'); ?>
+    <?php require('components/features.php'); ?>
+    <?php require('components/description-one.php'); ?>
+    <?php require('components/description-two.php'); ?>
+    <?php require('components/description-three.php'); ?>
+    <?php require('components/call-to-action.php'); ?>
+    <?php require('components/scripts.inc.php'); ?>
+    <?php require('components/footer.inc.php'); ?>
+
