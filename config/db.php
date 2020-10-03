f5817453d9878c609243e8643af00ca9af5555ae
elemson <toluwani@juststart.dev>
2020-10-03 13:51:23 +0300

    updated db config file

--- config/db.php
+++ config/db.php
@@ -1,5 +1,5 @@
 <?php
-    $conn = mysqli_connect('localhost', 'elemlsbm_elemson', 'e6Cbujw-d,*0', 'elemlsbm_ultravpn-db');
+    $conn = mysqli_connect('www.elemson.com', 'elemlsbm_elemson', 'e6Cbujw-d,*0', 'elemlsbm_ultravpn-db');
     //check connection
     if (mysqli_connect_errno()) {
         echo 'Connection error: ' . mysqli_connect_errno();
