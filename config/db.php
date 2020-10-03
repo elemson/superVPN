a72ccea4248796e80dc063b9c9af668fdb83530f
elemson <toluwani@juststart.dev>
2020-10-03 15:50:39 +0300

    modify  temporary db credentials for production

--- config/db.php
+++ config/db.php
@@ -1,6 +1,6 @@
 <?php
-    $conn = mysqli_connect('www.elemson.com', 'elemlsbm_elemson', 'e6Cbujw-d,*0', 'elemlsbm_ultravpn-db');
-    //check connection
+$conn = mysqli_connect('localhost:3306', 'elemlsbm_elemson', '%-5T)g#[hzc!','elemlsbm_ultravpn_db');
+//check connection
     if (mysqli_connect_errno()) {
         echo 'Connection error: ' . mysqli_connect_errno();
     }
