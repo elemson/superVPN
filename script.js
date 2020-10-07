e3ee95584dd9833f3f2cfba489e5d2216b87e0c4
elemson <toluwani@juststart.dev>
2020-10-07 22:32:25 +0300

    When page is loaded with query-parameter, it is redirected to offer link.

--- script.js
+++ script.js
@@ -1,23 +1,35 @@
+let URL = "https://track.ultravpn.com/5d02892a4faea/click";
+
+//If page loads with query parameter
+window.onload = function fetchClickIDOnLoad() {
+	const isQueryStringAbsent = window.location.href.indexOf('?') === -1;
+	if(!isQueryStringAbsent) {
+		document.location.search.substr(1).split('&').forEach(pair => {
+			[key, value] = pair.split('=')
+			URL += "/"
+			URL += value
+			window.location = URL;
+		})
+	}
+
+}
+
+//Function invokes when button is clicked
 function fetchClickID() {
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	const isClickID = (urlParams.has('clickid'))
-	const clickID = (urlParams.get('clickid'))
-	const isQueryStringAbsent = window.location.href.indexOf('?') == -1
-	let URL = "https://track.ultravpn.com/5d02892a4faea/click";
-	if (isClickID) {
+	if(!isClickID) {
+		urlParams.set('clickid', 123);
 		URL += "/";
-		URL += clickID;
-		return URL;
-	}	else if (isQueryStringAbsent) {
-		URL += "/";
-		URL += '123'
+		URL += urlParams.get('clickid');
+		console.log(URL)
 		return URL
 	}
-	return window.location.href
+
 }
 
-//Run when button is clicked
+//Runs when button is clicked
 function RedirectURL() {
 	window.location = fetchClickID();
 }
